dbadee9ed6e48b35986aaa50127f1f8fd2139f03
Jordan Bonnot <jojobonnot@gmail.com>
2019-04-11 17:24:39 +0200

    add viewcontroller + IBActions

---
+++ BDDMobile_List/Controllers/AddTagViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddTagViewController.swift
+//  BDDMobile_List
+//
+//  Created by lpiem on 11/04/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class AddTagViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+    }
+    
+    @IBAction func onDone(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
